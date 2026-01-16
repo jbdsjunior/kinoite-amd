0dcfc25caf462cbe46ec075fc287a553c12a232c
João Júnior <jbdsjunior@live.com>
2026-01-15 20:32:36 -0400

    fix packages

--- files/scripts/configure-updates.sh
+++ files/scripts/configure-updates.sh
@@ -1,10 +1,7 @@
 #!/bin/sh
-# Define a política para baixar atualizações em segundo plano (Stage)
-# O padrão 'check' apenas notifica, mas não prepara o sistema.
 
 sed -i 's/#AutomaticUpdatePolicy=none/AutomaticUpdatePolicy=stage/' /etc/rpm-ostreed.conf
 sed -i 's/AutomaticUpdatePolicy=check/AutomaticUpdatePolicy=stage/' /etc/rpm-ostreed.conf
 sed -i 's/AutomaticUpdatePolicy=none/AutomaticUpdatePolicy=stage/' /etc/rpm-ostreed.conf
 
-# Opcional: Permitir atualização mesmo se o PC não estiver ocioso
 sed -i 's/^#IdleExitTimeout=60/IdleExitTimeout=60/' /etc/rpm-ostreed.conf
